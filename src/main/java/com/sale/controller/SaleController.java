2825d51d000eaffd32c3404755b3d4dde5060599
shaikshahim786 <shaik.shahim786@gmail.com>
2025-11-01 08:41:25 +0530

    my first commit

---
+++ src/main/java/com/sale/controller/SaleController.java
@@ -0,0 +1,21 @@
+package com.sale.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sale.service.SaleService;
+
+@RestController
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+
+	@GetMapping("/getmessage")
+	public String getMessage(String message) {
+		int x = service.add(143, 2);
+		return "successfully sale the device with price " + x;
+	}
+
+}
---
+++ src/main/java/com/sale/service/SaleService.java
@@ -0,0 +1,16 @@
+package com.sale.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleService {
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+	
+	public int multiply(int x, int y) {
+		return x-y;
+	}
+
+}
