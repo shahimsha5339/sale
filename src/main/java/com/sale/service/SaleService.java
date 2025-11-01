c4ab3008be4d4c1b2126a4c98e97b17c5ab3de3f
shaikshahim786 <shaik.shahim786@gmail.com>
2025-11-01 11:53:44 +0530

    commit changes

--- src/main/java/com/sale/service/SaleService.java
+++ src/main/java/com/sale/service/SaleService.java
@@ -4,18 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SaleService {
-	
+
 	public int add(int x, int y) {
-		return x+y;
+		return x + y;
 	}
-	
-<<<<<<< HEAD
-	public int multiply( int y,int z) {
-		return y*z;
-=======
-	public int multiply(int x, int y) {
-		return x*y;
->>>>>>> f642e53d06bff8d08be14f77590f3338e15bce36
+
+	public int multiply(int y, int z) {
+		return y * z;
+
 	}
 
 }
